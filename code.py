17639d0a0f1073e1ea24524a65e13fa6e1c6ea4a
Eddy Yeo <edwardyeo@cmu.edu>
2013-03-21 04:48:24 -0400

    Fixed news page.

--- code.py
+++ code.py
@@ -15,7 +15,7 @@ class index:
 class news:
     def GET(self):
         db = web.database(dbn='sqlite', db='news.db')
-        articles = db.select('links')
+        articles = db.select('articles')
         return render.news(articles)
 
 class about:
